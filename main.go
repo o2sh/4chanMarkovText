4a35f30997124c2dd99850064f88b12db0383cf3
Ossama Hjaji <ossama-hjaji@live.fr>
2018-08-04 06:14:19 +0200

    end scrapper

--- main.go
+++ main.go
@@ -1,32 +1,37 @@
 package main
 
 import (
-	"fmt"
+	"bytes"
+	"io"
+	"os"
 	"strings"
 
-	api "github.com/moshee/go-4chan-api/api"
-	html "golang.org/x/net/html"
+	"github.com/moshee/go-4chan-api/api"
+	"golang.org/x/net/html"
 )
 
 func main() {
-	var board = "a"
+	var board = "pol"
+	var buffer bytes.Buffer
 	allThreadIdsPerBoard, err := api.GetThreads(board)
-	if err != nil {
-		panic(err)
-	}
-
-	for _, allThreadIdsPerPage := range allThreadIdsPerBoard {
+	check(err)
+	for _, allThreadIdsPerPage := range allThreadIdsPerBoard[0:4] {
 		for _, threadID := range allThreadIdsPerPage {
+
 			thread, err := api.GetThread(board, threadID)
-			if err != nil {
-				panic(err)
-			}
+			check(err)
 			for _, post := range thread.Posts {
-				fmt.Println(parser(post.Comment))
+				//fmt.Println(parser(post.Comment))
+				buffer.WriteString(parser(post.Comment))
+				buffer.WriteString("\n")
 			}
 
 		}
 	}
+
+	e := WriteStringToFile("./data/"+board+".txt", buffer.String())
+	check(e)
+	//fmt.Println(buffer.String())
 
 }
 
@@ -35,11 +40,28 @@ func parser(raw string) string {
 	domDocTest := html.NewTokenizer(strings.NewReader(raw))
 	for tokenType := domDocTest.Next(); tokenType != html.ErrorToken; {
 		TxtContent := strings.TrimSpace(html.UnescapeString(string(domDocTest.Text())))
-		if len(TxtContent) > 0 {
+		if len(TxtContent) > 5 && TxtContent[:2] != ">>" {
 			s = TxtContent
 		}
 		tokenType = domDocTest.Next()
 	}
 	return s
 
+}
+
+//WriteStringToFile dsdsdsd
+func WriteStringToFile(filepath, s string) error {
+	fo, err := os.Create(filepath)
+	check(err)
+	defer fo.Close()
+
+	_, err = io.Copy(fo, strings.NewReader(s))
+	check(err)
+	return nil
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }

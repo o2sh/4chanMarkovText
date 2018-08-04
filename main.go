79d02ab4b185cf5699cfc688b0a55c627462b497
Ossama Hjaji <ossama-hjaji@live.fr>
2018-08-04 08:45:35 +0200

    more inputs

--- main.go
+++ main.go
@@ -11,17 +11,18 @@ import (
 
 func main() {
 	//scrapp("pol")
+	//scrapp("fit")
 	//scrapp("b")
 	//scrapp("biz")
 	// Register command-line flags.
-	numWords := flag.Int("words", 52, "maximum number of words to print")
+	numWords := flag.Int("words", 100, "maximum number of words to print")
 	prefixLen := flag.Int("prefix", 2, "prefix length in words")
 
 	flag.Parse()                     // Parse command-line flags.
 	rand.Seed(time.Now().UnixNano()) // Seed the random number generator.
 
 	c := NewChain(*prefixLen) // Initialize a new Chain.
-	file, err := os.Open("./data/b.txt")
+	file, err := os.Open("./data/fit.txt")
 	check(err)
 	c.Build(bufio.NewReader(file)) // Build chains from standard input.
 	text := c.Generate(*numWords)  // Generate text.
